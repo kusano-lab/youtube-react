a4fc343cbacf2359771b4abb6d352299524bf74f
kusano-lab <i_am_the_voice@docomo.ne.jp>
2020-07-20 03:59:19 +0900

    fix top.js

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -3,21 +3,7 @@ import Layout from '../components/Layout/Layout'
 import { fetchPopularData } from '../api/index'
 import { v4 as uuid } from 'uuid'
 
-const Data = ({item}) => {
-  console.log(item)
-  let href = `www.youtube.com/watch?v=${item.id}`
-  return (
-    <li>
-      <a href={href}>
-        <img src={item.snippet.thumbnails.default.url} alt=""/>
-        <p>{item.snippet.title}</p>
-      </a>
-    </li>
-  )
-}
-
-
-const Top = () => {
+const VideoData = () => {
   const [items, setItems] = useState([]);
   
   useEffect(() => {
@@ -33,13 +19,25 @@ const Top = () => {
   }, [])
 
   return (
+    <ul>
+    {items.map(item => (
+      <li key={uuid()}>
+        <a href={`www.youtube.com/watch?v=${item.id}`}>
+          <img src={item.snippet.thumbnails.default.url} alt=""/>
+          <p>{item.snippet.title}</p>
+        </a>
+      </li>
+    ))}
+    </ul>
+  )
+}
+
+
+const Top = () => {
+  return (
     <Layout>
       <h1>Top page</h1>
-      <ul>
-      {items.map(item => {
-        return <Data key={uuid()} item={item} />
-      })}
-      </ul>
+      <VideoData />
     </Layout>
   )
 }
